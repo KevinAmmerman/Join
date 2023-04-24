496ed86027eb9fdc68bf80706062a239d80db20f
Kevin Ammerman <127098309+KevinAmmerman@users.noreply.github.com>
2023-04-24 17:51:44 +0200

    added dataBase.js with array structure

---
+++ src/scripts/dataBase.js
@@ -0,0 +1,32 @@
+let contacts = [
+    {
+        'name': 'Hans',
+        'email': 'hans.mueller@web.de',
+        'phone': '+49 89 7865975'
+    }
+];
+
+let tasks = {
+    toDo: [
+        {
+            'title': '',
+            'description': '',
+            'category': '',
+            'assignedTo': '',
+            'date': '',
+            'prio': '',
+            'subtask': '',
+        }
+    ],
+    inProgress: [],
+    feedBack: [],
+    done: []
+};
+
+let login = [
+    {
+        'name': '',
+        'email': '',
+        'password': ''
+    }
+];

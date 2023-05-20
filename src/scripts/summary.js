c4c2160321a7f6bea2d8f6b2ea93ca155db501ef
PascalMar <pa.marienfeld@gmail.com>
2023-05-20 12:46:01 +0200

    nothing changed

--- src/scripts/summary.js
+++ src/scripts/summary.js
@@ -1,75 +1,71 @@
 
 
-function getSummary(){
+function getSummary() {
     generateSummaryTemplate();
     loadUsers();
     greet();
-    
-    
+
+
 }
 
 
 
-function greetings(){
+function greetings() {
     const currentHour = new Date().getHours();
     let greetings = {
-    morning: 'Good morning,',
-    afternoon: 'Good afternoon,',
-    evening: 'Good evening,'
-  };
+        morning: 'Good morning,',
+        afternoon: 'Good afternoon,',
+        evening: 'Good evening,'
+    };
 
 
-    if(currentHour >= 4 && currentHour < 12){
+    if (currentHour >= 4 && currentHour < 12) {
         return greetings.morning;
     }
-     else if(currentHour >= 12 && currentHour < 18){
+    else if (currentHour >= 12 && currentHour < 18) {
         return greetings.afternoon;
     }
     else {
         return greetings.evening;
     }
-    };
+};
 
-
-
-
-
-function greet(){
+function greet() {
     loadUsers();
     const urlParams = new URLSearchParams(window.location.search);
     const userNames = urlParams.get('users');
     const guestNames = guestUser[0]['guestName'];
     let greetUser = document.getElementById('userSign');
     let greetinghtml = document.getElementById('greetings');
-        greetinghtml.innerHTML = greetings();
+    greetinghtml.innerHTML = greetings();
 
-    if(userNames){
-        
-         greetUser.innerHTML = `<span>${userNames}</span>`;
-        
-    } else { 
-            greetUser.innerHTML = `<span>${guestNames}</span>`;
+    if (userNames) {
+
+        greetUser.innerHTML = `<span>${userNames}</span>`;
+
+    } else {
+        greetUser.innerHTML = `<span>${guestNames}</span>`;
     }
-    if(urlParams.has('users')){
+    if (urlParams.has('users')) {
         greetUser.innerHTML = `<span>${userName}</span>`;
         localStorage.setItem('userName', userName);
-    }   else if(userName){
-            localStorage.getItem('userName', userName);
-            greetUser.innerHTML = `<span>${userName}</span>`;
-        }else{
-              greetUser.innerHTML = `<span>Guest</span>`;
-        }
-    
+    } else if (userName) {
+        localStorage.getItem('userName', userName);
+        greetUser.innerHTML = `<span>${userName}</span>`;
+    } else {
+        greetUser.innerHTML = `<span>Guest</span>`;
+    }
+
 }
 
 
-function deadlineTime(){
+function deadlineTime() {
     const time = new Date().getHours();
 
 }
 
 
-function goToBoard(){
+function goToBoard() {
     window.location.href = 'board.html';
 }
 
